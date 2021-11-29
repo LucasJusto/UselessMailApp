a89ccdeeb0480ca6a5649b4a86039d4da1fdb2be
Lucas Dimer Justo <37344488+LucasJusto@users.noreply.github.com>
2021-11-29 14:04:43 -0300

    xCode init

---
+++ UselessEmailApp/UselessEmailAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UselessEmailAppApp.swift
+//  UselessEmailApp
+//
+//  Created by Lucas Dimer Justo on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct UselessEmailAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
